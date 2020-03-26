bd9fe2f60ae6feecf97e28cb7799e8d7b7add301
Saad Kaleem <48062970+SaadKaleem@users.noreply.github.com>
2020-03-27 01:07:42 +0300

    Node JS Backend with Sudoku Algorithm
    Tested the Backend Server on localhost:3000/checksolution
    
    Body of POST request needs to be a JSON object in the form:
    
    {
      "grid": {
           "gridArr": [ [ ... ] , [ ... ] ]
         }
    }
    
    Output is boolean, True (if correctly solved) and False if not.

---
+++ node-js-sudoku/api/routes/checkSolution.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+var util = require("util");
+
+const spawn = require("child_process").spawn;
+
+router.get("/", (req, res, next) => {
+  res.status(200).json({
+    message: "Handling GET requests to /checksolution"
+  });
+});
+
+router.post("/", (req, res, next) => {
+
+  var gridToSend = JSON.stringify(req.body.grid.gridArr);
+
+  const pythonProcess = spawn("python.exe", ["./api/routes/sudoku.py", gridToSend]);
+
+  pythonProcess.stdout.on("data", function(data) {
+    
+    console.log(data.toString());
+    
+    res.status(201).write(data);
+    res.end('end');
+  });
+
+});
+
+module.exports = router;
